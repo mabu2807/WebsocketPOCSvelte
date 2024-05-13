396d2434a684862b2f287ef32e7c5e444a6a199c
johannadke <johanna.dke@web.de>
2024-05-13 14:00:11 +0200

    echo websocket poc

---
+++ src/lib/store.ts
@@ -0,0 +1,38 @@
+import { writable } from 'svelte/store';
+
+let socket: WebSocket | null = null;
+
+if (typeof window !== 'undefined') {
+	socket = new WebSocket('wss://echo.websocket.org');
+}
+const messageStore = writable('');
+
+if (socket) {
+	// Connection opened
+	socket?.addEventListener('open', function (event: Event) {
+		console.log("It's open");
+		console.log(event);
+	});
+
+	// Listen for messages
+	socket?.addEventListener('message', function (event) {
+		console.log('Message from server ', event.data);
+		messageStore.set(event.data);
+	});
+}
+
+const sendMessage = (message: string) => {
+	if (socket && socket.readyState <= 1) {
+		socket.send(message);
+	}
+};
+
+const storeMessage = (message: string) => {
+	messageStore.set(`You: ${message}`);
+};
+
+export default {
+	subscribe: messageStore.subscribe,
+	storeMessage,
+	sendMessage
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,38 @@
-<script>
-	import {browser} from '$app/environment';
-	
-import SockJS from 'sockjs-client';
-import Stomp from 'stompjs';
-	
-	if (browser) {
-		const socket = new SockJS('http://localhost:8080/ws');
-		socket.addEventListener("open", ()=> {
-			console.log("Opened")
-		})
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import store from '../lib/store.js';
+	import Message from './message.svelte';
+	let message: string;
+	let messages: string[] = [];
+
+	onMount(() => {
+		store.subscribe((currentMessage) => {
+			messages = [...messages, currentMessage];
+		});
+	});
+
+	function onSendMessage() {
+		if (message && message.length > 0) {
+			store.storeMessage(message);
+			store.sendMessage(message);
+			message = '';
+		}
 	}
-	</script>
+</script>
+
+<h1>Echo test</h1>
+
+<input type="text" bind:value={message} class="message-input" />
+<button on:click={onSendMessage}> Send Message </button>
+{#each messages as message, i}
+	<Message {message} direction={i % 2 == 0 ? 'right' : 'left'} />
+{/each}
+
+<style>
+	.message-input {
+		background-color: #f0f0f0; /* Hintergrundfarbe */
+		color: #333; /* Textfarbe */
+		border: 1px solid #ccc; /* Rahmenfarbe */
+		padding: 10px; /* Innenabstand */
+	}
+</style>
